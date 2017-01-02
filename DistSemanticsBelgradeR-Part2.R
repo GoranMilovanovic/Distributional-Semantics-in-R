c494f5658c649aa86948670e0805f35798d83bca
Goran S. Milovanović <goran.s.milovanovic@gmail.com>
2017-01-03 00:20:56 +0100

    Errata 01/03/2017

--- DistSemanticsBelgradeR-Part2.R
+++ DistSemanticsBelgradeR-Part2.R
@@ -7,10 +7,10 @@
 
 ### Part 2: Entity Recognition with {openNLP}
 
-### --- clear all
+### ----------------------------------------------------------------------
+### --- 2.1 clear all + libraries
 rm(list=ls())
 
-### ----------------------------------------------------------------------
 ### --- libraries
 
 # - helpers
@@ -37,9 +37,8 @@ library(RWeka) # load RWeka for tokenizers only after setting Options
 # - topic models via Latent Dirichlet Allocation
 library(topicmodels)
 
-# - entitity recognition via Apache openNLP
+# - entitity recognition via Apache OpenNLP
 library(openNLP)
-library(openNLPmodels.en)
 
 # - graphics
 library(ggplot2)
@@ -55,30 +54,36 @@ library(ape)
 # - parallelism
 library(snowfall)
 
-# - Load Shakespear's Plays
 ### ----------------------------------------------------------------------
+### --- 2.2 Load Shakespear's Plays
 
 # - Load Shakespeare's Plays as a {tm} Corpus
+outDir <- paste0(getwd(),"/FullCorpus")
+setwd(outDir)
 corpus <- readRDS('ShakespeareAnnotated.Rds')
 
 # - Document-level Metadata in corpus:
 corpus[[1]]$meta
+
 WScharacters <- meta(corpus, tag = "characters")
-head(WScharacters)
+head(WScharacters,2)
 
-# - Some preprocessing
-### ----------------------------------------------------------------------
-
+# - Macbeth:
 corpus[[19]]$meta$id
 macbeth <- corpus[[19]]$content
 class(macbeth)
 
+### ----------------------------------------------------------------------
+### --- 2.3 Some pre-processing
+  
 # - a small correction: CHARACTERS to Names:
 # - CHARACTERS to Titles w. {string}:
 candidateCharacters1 <- unique(str_extract_all(macbeth, "[[:upper:]]{2,}\\s[[:upper:]]{2,}")[[1]])
 candidateCharacters2 <- unique(str_extract_all(macbeth, "[[:upper:]]{2,}")[[1]])
 candidateCharacters1
+
 candidateCharacters2
+
 candidateCharacters <- c(candidateCharacters1, candidateCharacters2)
 # - clean up w. regex:
 candidateCharacters <- candidateCharacters[-which(grepl("^ACT|\\bACT\\b", candidateCharacters))]
@@ -87,92 +92,114 @@ candidateCharacters <- candidateCharacters[-which(grepl("^SCENE|\\bSCENE\\b", ca
 candidateCharacters <- candidateCharacters[-which(grepl("^I{1,3}$|^VI{1,3}$|^IV$|^V$|^IX$|^X$", candidateCharacters))]
 candidateCharacters
 
-# - implement:
 for (i in 1:length(candidateCharacters)) {
   macbeth <- str_replace_all(macbeth, candidateCharacters[i], str_to_title(candidateCharacters[i]))
 }
 
-# - {NLP} and {openNLP} structures
 ### ----------------------------------------------------------------------
-
+### --- 2.4 {NLP} and {openNLP} word and sentence annotations**
+  
 # - String class for {NLP} and {openNLP}
 macbeth <- as.String(macbeth)
 class(macbeth)
+
 macbeth[10,20] # subsetting string objects in {NLP}
+
 macbeth[102,178]
 
-# - Word and Sentence annotators:
 wordAnnotator <- Maxent_Word_Token_Annotator(language = "en") # {openNLP}
+wordAnnotator
+
 sentenceAnnotator <- Maxent_Sent_Token_Annotator(language = "en") # {openNLP}
-# - annotate Macbeth: N.B. use NLP::annotate to override ggplot2's annotate()!
+sentenceAnnotator
+
 annotatedMacbeth <- NLP::annotate(macbeth,
                                   list(sentenceAnnotator, wordAnnotator))
 class(annotatedMacbeth)
+
 length(annotatedMacbeth)
+
 annotatedMacbeth[[1]]
+
 annotatedMacbeth[[2]]
+
 annotatedMacbeth[[2]]$start
+
 macbeth[annotatedMacbeth[[2]]$start, annotatedMacbeth[[2]]$end]
-annotatedMacbeth[[1]]$id
-annotatedMacbeth[[1]]$type
-annotatedMacbeth[[1]]$start
-annotatedMacbeth[[1]]$end
-annotatedMacbeth[[2]]$features
-class(annotatedMacbeth[[2]]$features)
-annotatedMacbeth[[2]]$features[[1]]$constituents
+
+annotatedMacbeth[[1]]$features
+
+class(annotatedMacbeth[[1]]$features)
+
+annotatedMacbeth[[1]]$features[[1]]$constituents
 
 # $constituents:
 annMacbeth <- as.data.frame(annotatedMacbeth)
 head(annMacbeth)
+
 tail(annMacbeth)
+
 annMacbeth <- annMacbeth %>% 
   filter(type == "word")
 annotatedMacbeth[[1]]$features[[1]]$constituents
+
 length(annotatedMacbeth[[1]]$features[[1]]$constituents)
 # the constituents of annotatedMacbeth[[1]]: words in a sentence
 annMacbeth$id[1:8]
 
-# - AnnotatedPlainTextDocument
 macbethAPT <- AnnotatedPlainTextDocument(macbeth, annotatedMacbeth)
-macbethAPT
+class(macbethAPT)
+
 is.list(macbethAPT)
-macbethAPT$content
+
 length(macbethAPT$annotations[[1]])
+
 macbethAPT$annotations[[1]][[23274]]
+
 length(sents(macbethAPT)) # 1195
+
 is.list(sents(macbethAPT))
-sents(macbethAPT)[[1]]
+
+sents(macbethAPT)[[100]]
+
 class(sents(macbethAPT)[[1]])
-sents(macbethAPT)[[2]]
-sents(macbethAPT) %>% head(10)
+
+sents(macbethAPT) %>% head(5)
+
 words(macbethAPT) %>% head(100)
 
 # - clear
 rm(list = c('annMacbeth', 'annotatedMacbeth', 'macbethAPT'))
 
+### ----------------------------------------------------------------------
+### --- 2.5 Entity recognition with {openNLP}**
+  
 ### --- Entity Recognition from {openNLP}
 # - create an annotator:
 # - list of models available in openNLP 1.5 series:
 # - http://openNLP.sourceforge.net/models-1.5/
 # - visit: http://datacube.wu.ac.at/
-# install.packages("http://datacube.wu.ac.at/src/contrib/openNLPmodels.es_1.5-1.tar.gz", 
-#                  repos = NULL, 
-#                  type = "source")
-# install.packages("http://datacube.wu.ac.at/src/contrib/openNLPmodels.nl_1.5-2.tar.gz", 
-#                  repos = NULL, 
-#                  type = "source") 
+# - To install for Spanish:
+install.packages("http://datacube.wu.ac.at/src/contrib/openNLPmodels.es_1.5-1.tar.gz",
+                 repos = NULL,
+                 type = "source")
+# - To install for Dutch:
+install.packages("http://datacube.wu.ac.at/src/contrib/openNLPmodels.nl_1.5-2.tar.gz",
+                 repos = NULL,
+                 type = "source")
+
 characterAnnotatorEN <- Maxent_Entity_Annotator(language = "en", kind = "person")
 characterAnnotatorES <- Maxent_Entity_Annotator(language = "es", kind = "person")
 characterAnnotatorNL <- Maxent_Entity_Annotator(language = "nl", kind = "person")
+
 annotatedMacbeth <- NLP::annotate(macbeth,
                                   list(sentenceAnnotator,
                                        wordAnnotator,
                                        characterAnnotatorEN,
                                        characterAnnotatorES,
                                        characterAnnotatorNL))
-# - keep only person entity annotations:
-annotatedMacbeth <- annotatedMacbeth %>% 
-  as.data.frame %>%
+
+as.data.frame %>%
   filter(type == "entity")
 # - extract Shakespeare's characters from Macbeth:
 charactersMachbet <- str_sub(as.character(macbeth),
@@ -180,12 +207,12 @@ charactersMachbet <- str_sub(as.character(macbeth),
                              end = annotatedMacbeth$end) %>%
   unique()
 charactersMachbet
-# - compare:
+
 charactersMachbetCorpus <- unlist(strsplit(WScharacters[[19]],
                                            split = ", ", fixed = T)[[1]])
 foundCharacters <- charactersMachbetCorpus[which(charactersMachbetCorpus %in% charactersMachbet)]
 foundCharacters
-# - accuracy:
+
 acc <- round((length(foundCharacters)/length(charactersMachbetCorpus))*100,2)
 acc
 
@@ -195,13 +222,17 @@ charactersMachbet <- unique(c(charactersMachbet,
                                 unlist(strsplit(charactersMachbet, 
                                                 split = " ", 
                                                 fixed = T)))
-                              )
-                            )
-foundCharacters <- charactersMachbetCorpus[which(charactersMachbetCorpus %in% charactersMachbet)]
+)
+)
+foundCharacters <- 
+  charactersMachbetCorpus[which(charactersMachbetCorpus %in% charactersMachbet)]
 foundCharacters
-# - accuracy:
+
 acc <- round((length(foundCharacters)/length(charactersMachbetCorpus))*100,2)
 acc
+
+### ----------------------------------------------------------------------
+### --- 2.6 The whole Shakespeare undergoes {openNLP} entity recognition...
 
 # - Annotate all plays
 ### ----------------------------------------------------------------------
@@ -209,7 +240,7 @@ recognized <- character()
 accuracy <- numeric()
 # - a directory where annotated plays will be saved as .Rds files
 setwd(paste0(getwd(),"/FullCorpus/openNLPAnnotations"))
-for (i in 7:length(corpus)) {
+for (i in 1:length(corpus)) {
   print(paste0("Processing play ", i, ". out of 37..."))
   play <- corpus[[i]]$content
   # - CHARACTERS to Titles w. {string}:
@@ -244,7 +275,7 @@ for (i in 7:length(corpus)) {
   saveRDS(annotatedPlay, file = paste0(corpus[[i]]$meta$id,
                                        "_Annotated.Rds"))
   
-  annotatedPlay %>%
+  annotatedPlay <- annotatedPlay %>%
     as.data.frame() %>%
     filter(type == "entity")
   
@@ -277,8 +308,6 @@ for (i in 7:length(corpus)) {
   
 }
 
-# - extract data.frame for analytics
-
 charRecognition <- data.frame(play = as.character(meta(corpus, tag = "id")),
                               recognized = recognized,
                               accuracy = accuracy,
@@ -292,18 +321,33 @@ charRecognition$numCharsCorpus <- sapply(meta(corpus, tag = "characters"), funct
 })
 write.csv(charRecognition, file = "charRecognition.csv")
 
-# - Results
 ### ----------------------------------------------------------------------
+### --- 2.7 Evaluation: how well does {openNLP} perform against Shakespeare's plays?
+
+# - Results
+
+rm(list=ls())
+library(dplyr)
+library(ggplot2)
+charRecognition <- read.csv("charRecognition.csv",
+                            header = T,
+                            row.names = 1,
+                            stringsAsFactors = F)
+head(charRecognition)
 
 summary(charRecognition$accuracy)
+
 charRecognition$recognized[26] # Romeo and Juliet
+
 charRecognition$recognized[19] # Macbeth
+
 charRecognition$recognized[15] # Julius Caesar
 
-# - plot accuracy
-ggplot(data = charRecognition,
-       aes(accuracy)) +
-  geom_line(stat="density", color = "firebrick4") + 
+# - accuracy density plot 
+ggplot(data = charRecognition) +
+  geom_line(aes(accuracy), stat="density", color = "black", linetype = 2) + 
+  geom_line(aes(accuracy, color = type), stat="density") + 
+  scale_colour_manual(values = c("cadetblue4","firebrick4", "darkorchid4")) +
   theme_classic() +
   ggtitle("{openNLP} Named Entity Recognition\nShakespeare's Plays") +
   theme(axis.line.y = element_blank()) +
@@ -311,12 +355,12 @@ ggplot(data = charRecognition,
   theme(plot.title = element_text(size = 9, hjust = .5)) +
   xlim(0,1)
 
-# - plot accuracy by type 1
+# - plot accuracy by type
 ggplot(data = charRecognition,
        aes(x = type, y = accuracy, color = type)) +
   scale_colour_manual(values = c("cadetblue4","firebrick4", "darkorchid4")) +
   geom_jitter(aes(alpha = accuracy), size = 3.5, width = .1) + 
-  ylim(0, 1) + xlab(NULL) + ylab("Accuracy") + 
+  ylim(0, max(charRecognition$accuracy)+.2) + xlab(NULL) + ylab("Accuracy") + 
   ggtitle("{openNLP} Named Entity Recognition\nShakespeare's Plays") + 
   theme_classic() + 
   theme(axis.line.y = element_blank()) +
@@ -326,47 +370,118 @@ ggplot(data = charRecognition,
 # - accuracy by type
 charRecognition %>% 
   group_by(type) %>% 
-  summarise(typeMean = mean(accuracy), 
-            typeSD = sd(accuracy),
+  summarise(MeanAcc = mean(accuracy), 
+            StDevAcc = sd(accuracy),
             meanNumChar = mean(numCharsCorpus)) %>% 
-  arrange(desc(typeMean))
+  arrange(desc(MeanAcc))
 
 # - plot accuracy by type 2
 ggplot(data = charRecognition,
-       aes(x = numCharsFound, y = numCharsCorpus, color = type)) +
+       aes(x = numCharsCorpus, y = accuracy, color = type)) +
   scale_colour_manual(values = c("cadetblue4","firebrick4", "darkorchid4")) + 
   theme_classic() +
   geom_smooth(method = lm, alpha = .05) +
   geom_point(aes(alpha = accuracy)) +
-  ylim(0, max(charRecognition$numCharsCorpus+10)) + 
-  xlim(0, max(charRecognition$numCharsFound+10)) +  
-  xlab("{openNLP} Recognized Characters") + ylab("Characters in Corpus") + 
+  ylim(0, max(charRecognition$accuracy)+.2) + 
+  xlim(0, 70) +  
+  ylab("Accuracy") + xlab("Characters in Corpus") + 
   ggtitle("{openNLP} Named Entity Recognition\nShakespeare's Plays") +
   theme(axis.line.y = element_blank()) +
   theme(axis.line.x = element_blank()) +
   theme(plot.title = element_text(size = 9, hjust = .5))
 
-# - The following finding is due to the availability of 
-# - language models: EN, ES, NL, for personal names recognition
-charRecognition$type <- factor(charRecognition$type,
-                               levels = c('History', 'Comedy', 'Tragedy'))
+### --- Logistic Regression over Acc (proportion correct)
 
 playsFit1 <- 
-  glm(accuracy ~ type + type:numCharsCorpus,
-      weights = numCharsCorpus,
+  glm(cbind(numCharsFound, numCharsCorpus-numCharsFound) ~ type + type:numCharsCorpus,
+      family = binomial(logit),
       data = charRecognition)
 summary(playsFit1)
+
 exp(playsFit1$coefficients)
 
-playsFit2 <-
-  glm(accuracy ~ type,
-      weights = numCharsCorpus,
+playsFit2 <- 
+  glm(cbind(numCharsFound, numCharsCorpus-numCharsFound) ~ type,
+      family = binomial(logit),
       data = charRecognition)
 summary(playsFit2)
+
 exp(playsFit2$coefficients)
 
-# - model selection
-playsFit1$aic
-playsFit2$aic
+# - model selection: compare model Akaike's Information Criteria:
+playsFit1$aic < playsFit2$aic
+
+### --- Why did the name recognition failed for histories 
+### --- in comparison to tragedies and comedies?
+
+wDir <- paste0(getwd(),'/FullCorpus')
+setwd(wDir)
+playList2 <- read.csv('playList2.csv',
+                      header = T,
+                      check.names = F,
+                      stringsAsFactors = F)
+str(playList2)
+
+# - clear up playList2 a bit:
+library(stringr)
+playList2$Genre <- str_trim(playList2$Genre, side = "right")
+playList2$City <- str_trim(playList2$City, side = "right")
+playList2$Country_Modern <- str_trim(playList2$Country_Modern, side = "right")
+playList2$CountryNote <- str_trim(playList2$CountryNote, side = "right")
+head(playList2)
+
+# - Load Shakespeare's Plays as a {tm} Corpus
+library(tm)
+# - Load Shakespeare's Plays as a {tm} Corpus
+outDir <- paste0(getwd(),"/FullCorpus")
+setwd(outDir)
+corpus <- readRDS('ShakespeareAnnotated.Rds')
+
+# - new metadata
+meta(corpus, tag = "description2", type = "local") <- 
+  unname(sapply(meta(corpus, tag = "id"),
+                function(x) {
+                  wPlay <- which(playList2$Title == as.character(x))
+                  playList2$Genre
+                }))
+meta(corpus, tag = "city", type = "local") <- 
+  unname(sapply(meta(corpus, tag = "id"),
+                function(x) {
+                  wPlay <- which(playList2$Title == as.character(x))
+                  playList2$City
+                }))
+meta(corpus, tag = "country", type = "local") <- 
+  unname(sapply(meta(corpus, tag = "id"),
+                function(x) {
+                  wPlay <- which(playList2$Title == as.character(x))
+                  playList2$Country_Modern
+                }))
+meta(corpus, tag = "countryNote", type = "local") <- 
+  unname(sapply(meta(corpus, tag = "id"),
+                function(x) {
+                  wPlay <- which(playList2$Title == as.character(x))
+                  playList2$CountryNote
+                }))
+
+### --- writeCorpus()
+# - Load Shakespeare's Plays as a {tm} Corpus
+outDir <- paste0(getwd(),"/FullCorpus")
+setwd(outDir)
+saveRDS(corpus, file = "ShakespeareAnnotated.Rds")
+
+type <- unlist(meta(corpus, tag = "description"))
+wComedy <- which(type == "Comedy")
+wTragedy <- which(type == "Tragedy")
+wHistory <- which(type == "History")
+
+comedyCountries <- unname(unlist(meta(corpus, tag = "country")))[wComedy]
+table(comedyCountries)
+
+tragedyCountries <- unname(unlist(meta(corpus, tag = "country")))[wTragedy]
+table(tragedyCountries)
+
+historyCountries <- unname(unlist(meta(corpus, tag = "country")))[wHistory]
+table(historyCountries)
+
 
 ### - Visit my blog :: [The Exactness of Mind](http://www.exactness.net), 12/24/2016.  
